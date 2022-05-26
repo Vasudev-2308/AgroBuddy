e0f49ef41219916875accbe380978e7f9c5b4bea
Vasudev-2308 <kingzvasu660@gmail.com>
2022-05-26 20:24:00 +0530

    Made Changes, Recommender is Working

--- plant_prediction/app.py
+++ plant_prediction/app.py
@@ -13,13 +13,36 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
 
-    features = [x for x in request.form.values()]
-    #final_features = [np.array(list(features))]
-    
-    # prediction = model.predict(final_features)
-    # output = prediction[0]
+    features = []
+    n_val = request.form.get('n_val')
+    p_val = request.form.get('p_val')
+    k_val = request.form.get('k_val')
+    temp_val = request.form.get('temp_val')
+    hum_val = request.form.get('humid_val')
+    ph_val = request.form.get('ph_val')
+    rf_val = request.form.get('rf_val')
 
-    return render_template('basic.html', prediction_text='Crop will be {}'.format(features))
+
+    
+
+    features.append(float(n_val))  # type: ignore
+    features.append(float(p_val))  # type: ignore
+    features.append(float(k_val))  # type: ignore
+    features.append(float(temp_val)) # type: ignore
+    features.append(float(hum_val)) # type: ignore
+    features.append(float(ph_val)) # type: ignore
+    features.append(float(rf_val)) # type: ignore
+
+    
+    
+    
+    
+    final_features = [np.array(features)]
+    
+    prediction = model.predict(final_features)
+    output = prediction[0]
+
+    return render_template('basic.html', prediction_text='Crop will be {}'.format(output))
 
 
 
